30d12add1a947888ea05c441d9d3d1268205edc6
Livia <llabate@npr.org>
2015-03-16 17:17:39 -0400

    renamed file, handle URL special cases

--- scraper.py
+++ scraper.py
@@ -1,32 +1,34 @@
-import urlparse
-import csv
-import os
-import re
 import codecs
+import csv
+import urlparse
 
-from lxml.html import fromstring
 from scrapelib import Scraper, FileCache
-import unicodecsv
 
 s = Scraper(requests_per_minute=60)
-# s.cache_storage = FileCache('walmart_cache')
-# s.cache_write_only = False
+s.cache_storage = FileCache('walmart_cache')
+s.cache_write_only = False
 
 def read_csv():
 	with open('scrapedsearch.csv', 'rb') as f:
-		reader = csv.DictReader(f, fieldnames=["link", "title", "emptytitle", "pr_date", "description", "page_number"])
+		reader = csv.DictReader(f, fieldnames=["title", "link", "date", "description"])
 		for row in reader:
 			print row
-			if row['link'] != 'link_1':
+			if row['link'] != 'link':
 				scrape_release(row)
 
 def scrape_release(row):
 	path = urlparse.urlparse(row['link'])[2]
 	components = path.split('/')
-	year = components[2]
-	month = components[3]
-	day = components[4]
-	slug = components[5]
+	if components[1] == 'news-archive':		
+		year = components[2]
+		month = components[3]
+		day = components[4]
+		slug = components[5]
+	else:
+		year = components[1]
+		month = components[2]
+		day = components[3]
+		slug = components[4]
 
 	response = s.urlopen(row['link'], retry_on_404=True)
 	f = codecs.open('data/%s-%s-%s-%s.txt' % (year, month, day, slug),'w', encoding='utf-8')
