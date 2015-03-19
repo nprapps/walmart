be58c9e0c2bda99c7d131ed8c10b9683de293eb2
Tyler Fisher <tyler@tylerjfisher.com>
2015-03-19 16:56:59 -0400

    a much cleaner query

--- calculate.sh
+++ calculate.sh
@@ -6,32 +6,14 @@ psql walmart -c "ALTER TABLE stores ADD COLUMN geom geometry(POINT,4326);"
 psql walmart -c "UPDATE stores SET geom = ST_SetSRID(ST_MakePoint(longitude,latitude),4326);"
 psql walmart -c "CREATE INDEX idx_stores_geom ON stores USING GIST(geom)"
 
-echo "Calculate centroids of census blocks"
-psql walmart -c "ALTER TABLE blocks ADD centroid_x double precision;"
-psql walmart -c "ALTER TABLE blocks ADD centroid_y double precision;"
-psql walmart -c "ALTER TABLE blocks ADD COLUMN centroid geometry(POINT,4326);"
-psql walmart -c "UPDATE blocks SET centroid_x= ST_X(ST_Centroid(wkb_geometry));"
-psql walmart -c "UPDATE blocks SET centroid_y= ST_Y(ST_Centroid(wkb_geometry));"
-psql walmart -c "UPDATE blocks SET centroid = ST_SetSRID(ST_MakePoint(centroid_x,centroid_y),4326);"
-psql walmart -c "CREATE INDEX idx_blocks_centroid ON blocks USING GIST(centroid)"
+echo "Create table of each block's distance to a Walmart"
 
-echo "Create view of all centroid/Walmart relationships < 5km"
-psql walmart -c "CREATE OR REPLACE VIEW allblocks_5km AS
-    SELECT DISTINCT B.ogc_fid as id, B.pop10 as population, ST_Distance_Sphere(A.geom, B.centroid) as distance
-    FROM stores AS A, blocks as B
-    WHERE A.gid IN (
-        SELECT X.gid
-        FROM stores as X, blocks as Y
-        WHERE X.state = 'HI' AND X.gid=A.gid
-        ORDER BY ST_Distance_Sphere(X.geom, Y.centroid) ASC LIMIT 1
-    ) AND B.pop10 > 0 AND ST_Distance_Sphere(A.geom, B.centroid) < 5000 ORDER BY B.ogc_fid;"
-psql walmart -c "COPY(select * from allblocks_5km) to '`pwd`/build/blocks-to-walmart-5km.csv' WITH CSV HEADER;"
-
-echo "Create view of unique blocks within 5km of a Walmart"
-psql walmart -c "CREATE OR REPLACE VIEW unique_blocks_5km AS
-    SELECT id, population, min(distance)
-    FROM allblocks_5km GROUP BY id, population;"
-psql walmart -c "COPY(select * from unique_blocks_5km) to '`pwd`/build/unique-blocks-5km.csv' WITH CSV HEADER;"
-
-echo "Find total number of people within x miles of a Walmart"
-psql walmart -c "select (select sum(unique_blocks_5km.population) from unique_blocks_5km) / (select sum(blocks.pop10) from blocks);"
+psql walmart -c "DROP TABLE unique_blocks_5km"
+psql walmart -c "CREATE TABLE unique_blocks_5km AS
+    SELECT
+        Y.ogc_fid as block_id,
+        Y.pop10 as population,
+        min(ST_Distance_Sphere(X.geom, ST_Centroid(Y.wkb_geometry))) as distance
+    FROM stores as X, blocks as Y
+    WHERE X.state = 'DC'
+    GROUP BY block_id, population;"
