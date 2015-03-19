f38b7e3d92d7c0b3568fbc702cedcef631495c46
Tyler Fisher <tyler@tylerjfisher.com>
2015-03-19 10:17:43 -0400

    fix nearest walmart query

--- calculate.sh
+++ calculate.sh
@@ -17,14 +17,14 @@ psql walmart -c "CREATE INDEX idx_blocks_centroid ON blocks USING GIST(centroid)
 
 echo "Find nearest Walmart for every centroid"
 psql walmart -c "CREATE OR REPLACE VIEW blocks_5km AS
-    SELECT DISTINCT B.ogc_fid, B.pop10 as population, ST_Distance_Sphere(A.geom, B.centroid) as distance
+    SELECT DISTINCT B.ogc_fid, B.pop10 as population
     FROM stores AS A, blocks as B
     WHERE A.gid IN (
         SELECT X.gid
         FROM stores as X, blocks as Y
         WHERE X.state = 'HI' AND X.gid=A.gid
         ORDER BY ST_Distance_Sphere(X.geom, Y.centroid) ASC LIMIT 1
-    ) AND B.pop10 > 0 AND ST_Distance_Sphere(A.geom, B.centroid) < 5000 ORDER BY B.ogc_fid LIMIT 1;"
+    ) AND B.pop10 > 0 AND ST_Distance_Sphere(A.geom, B.centroid) < 5000 ORDER BY B.ogc_fid;"
 
 psql walmart -c "COPY(select * from blocks_5km) to '`pwd`/build/blocks_in_range.csv' WITH CSV HEADER;"
 
